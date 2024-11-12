baa22d544b813a0b68cfaaaf7fe8074c0f5e82f5
Ruslan Gafarov <gafarov-ruslan@internet.ru>
2024-11-12 03:19:56 +0300

    Добавьте файлы проекта.

---
+++ increase_motivation/increase_motivation.cpp
@@ -0,0 +1,27 @@
+﻿#include <tgbot/tgbot.h>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+//функция вытягивания цитат из текстового файла
+vector<string> loadQuotesFromFile(const string& filePath) {
+    vector<string> quotes;
+    ifstream file(filePath);
+    string line;
+
+    while (getline(file, line)) {
+        if (!line.empty())
+            quotes.push_back(line);
+    }
+    return quotes;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+}
+
