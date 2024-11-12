e56aa2c281daae1c145301e51c3c9302dfcd92a0
Ruslan Gafarov <gafarov-ruslan@internet.ru>
2024-11-13 00:56:34 +0300

    реализована функция вытягивания пользователей из БД

--- increase_motivation/increase_motivation.cpp
+++ increase_motivation/increase_motivation.cpp
@@ -9,7 +9,7 @@ using namespace std;
 using namespace TgBot;
 using namespace chrono;
 
-//функция вытягивания токена из файла
+//вытягивание токена из файла
 string getToken(const string& filePath) {
     ifstream file(filePath);
     string token;
@@ -19,7 +19,7 @@ string getToken(const string& filePath) {
 
     return token;
 }
-//функция вытягивания цитаты
+//вытягивание цитаты из файла
 vector<string>loadQuotes(const string& filePath) {
     ifstream file(filePath);
     vector<string> quotes;
@@ -47,6 +47,21 @@ void addUser(sqlite3* db, int64_t chatId) {
     if (sqlite3_exec(db, sql.c_str(), 0, 0, nullptr) != SQLITE_OK)
         throw runtime_error("Failed to add user");
 }
+//вытягивание пользователей из БД
+vector<int64_t>getUser(sqlite3* db) {
+    vector<int64_t> chatIds;
+    const char* sql = "SELECT ChatId FROM Users;";
+    sqlite3_stmt* stmt;
+
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, 0) == SQLITE_OK) {
+        while (sqlite3_step(stmt) == SQLITE_ROW)
+            chatIds.push_back(sqlite3_column_int64(stmt, 0));
+    }
+
+    sqlite3_finalize(stmt);
+    return chatIds;
+}
+
 int main() {
     setlocale(LC_ALL, "Ru");
 
